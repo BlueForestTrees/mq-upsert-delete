6254b978eaecfe77ce214dfcd97862636b2771d5
slim <smedini@gmail.com>
2019-03-28 16:30:03 +0100

    2.0.9 - snackerrors

--- src/upsert-delete.js
+++ src/upsert-delete.js
@@ -1,29 +1,18 @@
 import ENV from "./env"
 import {dbInit, col} from "mongo-registry"
 import {initRabbit, createReceiver} from "simple-rbmq"
+import {del, upsert} from "./work"
+
+const jobs = [
+    {routingKey: `${ENV.NAME}-upsert`, queue: {...ENV.QUEUE, name: `${ENV.NAME}-upsert`}, work: upsert},
+    {routingKey: `${ENV.NAME}-delete`, queue: {...ENV.QUEUE, name: `${ENV.NAME}-delete`}, work: del}
+]
 
 dbInit(ENV)
     .then(() => initRabbit(ENV.RB))
     .then(() => col(ENV.DB_COLLECTION))
-    .then(db => Promise.all([
-        initRobot("upsert", db, upsert),
-        initRobot("delete", db, del)
-    ]))
-
-const initRobot = (action, db, work) =>
-    createReceiver(
-        ENV.RB.exchange,
-        `${ENV.NAME}-${action}`,
-        {...ENV.QUEUE, name: `${ENV.NAME}-${action}`},
-        msg => work(msg, db)
-    )
-
-const upsert = (doc, db) => db.updateOne(
-    {_id: doc._id},
-    {$set: doc},
-    {upsert: true}
-)
-
-const del = (doc, db) => db.deleteOne(
-    {_id: doc._id}
-)
+    .then(db => Promise.all(
+        jobs.map(
+            job => createReceiver(ENV.RB.exchange, job.routingKey, job.queue, msg => job.work(msg, db))
+        )
+    ))
---
+++ src/work.js
@@ -0,0 +1,9 @@
+export const upsert = (doc, db) => db.updateOne(
+    {_id: doc._id},
+    {$set: doc},
+    {upsert: true}
+)
+
+export const del = (doc, db) => db.deleteOne(
+    {_id: doc._id}
+)
